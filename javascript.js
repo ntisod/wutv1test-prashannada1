fd783829e30bcc8fd3cc7a0002f4d00fe13152ea
prashannada1 <prashannasigdel12@gmail.com>
2025-03-13 11:20:08 +0100

    uppgift 1,2,3

---
+++ javascript.js
@@ -0,0 +1,8 @@
+/* gjört 2025-03-13 */
+
+
+    function changeTitle() {
+        document.getElementById("rubrik").innerText = "Prashanna Sigdel"; 
+        document.getElementById("rubrik").classList.add("highlight"); 
+    }
+    
